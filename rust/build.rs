a585158961c515c7bb89704e7ef55d7cfb1372ec
str4d <jack@electriccoin.co>
2023-05-23 15:01:13 +0100

    Rust updates (#93)
    * cargo update
    
    * Migrate to `ffi_helpers 0.3`
    
    * Migrate to `cbindgen 0.24`
    
    * Migrate to Rust 2021 edition
    
    * Add `zcashlc_init_on_load` FFI method
    
    This hooks up the Rust logging system to the Apple OS logging framework,
    and explicitly names the Rayon threads for debugging visibility.
    
    * `make xcframework`

--- rust/build.rs
+++ rust/build.rs
@@ -1,8 +1,27 @@
 extern crate cbindgen;
 
-use std::env;
+use std::{env, path::PathBuf};
 
 fn main() {
+    println!("cargo:rerun-if-changed=rust/wrapper.c");
+    println!("cargo:rerun-if-changed=rust/wrapper.h");
+
+    let bindings = bindgen::builder()
+        .header("rust/wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .allowlist_function("os_log_.*")
+        .allowlist_function("os_release")
+        .allowlist_function("os_signpost_.*")
+        .generate()
+        .expect("should be able to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("should be able to write bindings");
+
+    cc::Build::new().file("rust/wrapper.c").compile("wrapper");
+
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     if let Ok(b) = cbindgen::Builder::new()
